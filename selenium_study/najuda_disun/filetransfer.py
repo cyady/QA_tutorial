eba40ea38777cd745b4482ceb757759126d1c00d
cyady <ekgh821@hanmail.net>
2023-07-13 23:51:16 +0900

    [23.07.13] pywinauto를 활용해서 파일변환
    버튼눌러서 새로 열린 창을 제대로 인식 못한다

--- selenium_study/najuda_disun/filetransfer.py
+++ selenium_study/najuda_disun/filetransfer.py
@@ -1,12 +1,40 @@
 #pip install pywinauto
 
+from pywinauto import application, findwindows, keyboard
+import pyperclip, time
+
 
 import os
 import olefile
 import subprocess
-from pywinauto import application
-app = application.Application()
+
+app = application.Application(backend='uia')
 
 hwpconv="C:\Program Files\Microsoft Office\Office15\BATCHHWPCONV.EXE"
 app.start(hwpconv)
+
+procs = findwindows.find_elements()
+
+for proc in procs:
+    print(f"{proc} / process : {proc.process_id}")
+
+dlg = app['Microsoft Word를 위한 아래아한글 문서 일괄 변환 도구']
+
+dir_hwp = r"C:\Users\cyady\Downloads\\7.10특징주 (1).hwp"
+pyperclip.copy(dir_hwp)
+
+dlg["RadioButton2"].click()
+dlg["...Button"].click()
+
+time.sleep(0.1)
+
+dlg2.print_control_identifiers()
+
+
+dlg2["Toolbar4"].click()
+
+
+keyboard.send_keys('^v')    #ctrl + v
+keyboard.send_keys('{ENTER}')
+
 
