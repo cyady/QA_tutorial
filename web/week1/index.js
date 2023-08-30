baf2a4abbc7755fdad73ab46a760e4ee8046d078
cyady <ekgh821@hanmail.net>
2023-08-30 20:45:26 +0900

    [23.08.30]강의 이어듣기 전일 미저장

--- web/week1/index.js
+++ web/week1/index.js
@@ -34,6 +34,24 @@ app.get('/user/:id', (req, res) => {
     res.json({'userid':q.name})
 })
 
+app.get('/sound/:name', (req, res) => {
+    const {name} = req.params
+
+    if(name =="dog") {
+        res.json({'sound':'멍멍'})
+    }
+    else if(name =="cat") {
+        res.json({'sound':'야옹'})
+    }
+    else if(name =="pig") {
+        res.json({'sound':'꿀꿀'})
+    }
+    else {
+        res.json({'sound':'알수없음'})
+    }
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
